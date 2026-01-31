095b644b9e2534e58fe4c9f49fc2823624194ca5
loetmeister <loetmeister@habmalnefrage.de>
2026-01-31 22:23:56 +0100

    new experimental device HBW-Sen-DB-4_dial

---
+++ HBW-Sen-DB-4_dial/HBW-Sen-DB-4_dial.ino
@@ -0,0 +1,155 @@
+//*******************************************************************
+//
+// HBW-Sen-DB-4
+//
+// Homematic Wired Hombrew Hardware
+// Arduino NANO als Homematic-Device
+// Türklingelsensor mit 4 Tastern und Beleuchtung des Klingeltableau
+// 
+// http://loetmeister.de/Elektronik/homematic/index.htm#modules
+//
+//*******************************************************************
+// Changes
+// v0.10
+// - initial version
+// v0.20
+// - fix for repeatCounter reset handling
+// v0.3
+// - added buzzer for button feedback (uses tone() builtin function) - needs new XML due to additional config
+// v0.4
+// - added peering for backlight chan - needs new XML due to updated config!
+// v0.5
+// - new variant with phone dial module
+
+
+#define HARDWARE_VERSION 0x02
+#define FIRMWARE_VERSION 0x0032
+#define HMW_DEVICETYPE 0x98 //device ID (make sure to import hbw-dis-key-4.xml into FHEM)
+
+// + 1 türsummer?
+#define NUMBER_OF_DIM_CHAN 1   // dimmer output (for backlight) - with auto_brightness
+#define NUMBER_OF_KEY_CHAN 4   // knobs at your door
+#define PHONE_DIAL 1  // phone connected (if used 1 or 0 if not used)
+#define NUM_LINKS_KEY 20
+#define NUM_LINKS_DIM 20
+#define LINKADDRESSSTART_DIM 0x40  // any actor peering!
+#define LINKADDRESSSTART_KEY 0x13C  // any sensor peering!
+
+
+// HB Wired protocol and module
+#include <HBW_eeprom.h>
+#include <HBWired.h>
+#include <HBWLinkKey.h>
+#include "HBWPhoneDial.h"
+#include "HBWKeyDoorbell.h"
+#include <HBWDimBacklight.h>
+#include <HBWLinkSwitchSimple.h>
+
+
+// Pins and hardware config
+#include "HBW-Sen-DB-4_config_example.h"  // When using custom device pinout or controller, copy this file and include it instead
+
+
+#define NUMBER_OF_CHAN NUMBER_OF_DIM_CHAN + NUMBER_OF_KEY_CHAN + PHONE_DIAL
+
+struct hbw_config {
+  uint8_t logging_time;     // 0x01
+  uint32_t central_address;  // 0x02 - 0x05
+  uint8_t direct_link_deactivate:1;   // 0x06:0
+  uint8_t              :7;   // 0x06:1-7
+  hbw_config_dim_backlight BlDimCfg[NUMBER_OF_DIM_CHAN]; // 0x07 - 0x08 (address step 2)
+  hbw_config_key_doorbell DBKeyCfg[NUMBER_OF_KEY_CHAN]; // 0x09 - 0x20 (address step 6)
+  hbw_config_phone_dial PhoneDialCfg[1];  // 0x21 - step 12 (not all parameters defined yet)
+} hbwconfig;
+
+
+HBWChannel* channels[NUMBER_OF_CHAN];  // total number of channels for the device
+
+HBWDevice* device = NULL;
+
+#if (PHONE_DIAL == 1)
+// SHIFT_REGISTER_CLASS defined in "PhoneDial.h"
+// init and reset shift register (OE will be cleared later)
+SHIFT_REGISTER_CLASS* myShiftReg = new SHIFT_REGISTER_CLASS(shiftReg_Data, shiftReg_Clock, shiftReg_Latch);
+#endif
+
+void setup()
+{
+  channels[0] = new HBWDimBacklight(&(hbwconfig.BlDimCfg[0]), BACKLIGHT_PWM, LDR_PIN);
+
+  #if (PHONE_DIAL == 1)
+  HBWPhoneDial* phone_dial_chan = new HBWPhoneDial(&(hbwconfig.PhoneDialCfg[0]), myShiftReg, NUMBER_OF_DIM_CHAN, LINE_INDICATOR_PIN);
+  channels[NUMBER_OF_CHAN -1] = phone_dial_chan;
+  #endif
+  
+  static const byte BUTTON_PIN[] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4};
+  
+ #if (NUMBER_OF_KEY_CHAN == 4)
+  for(uint8_t i = 0; i < NUMBER_OF_KEY_CHAN; i++) {
+    #if (PHONE_DIAL == 1)
+    channels[i + NUMBER_OF_DIM_CHAN] = new HBWKeyDoorbell(BUTTON_PIN[i], &(hbwconfig.DBKeyCfg[i]), phone_dial_chan, BUZZER);
+    #else
+    channels[i + NUMBER_OF_DIM_CHAN] = new HBWKeyDoorbell(BUTTON_PIN[i], &(hbwconfig.DBKeyCfg[i]), BUZZER);
+    #endif
+    
+  }
+ #else
+  #error NUMBER_OF_KEY_CHAN channel missmatch!
+ #endif
+  
+  #if (PHONE_DIAL == 1)
+  // set shift register OE pin LOW, this enables the outputs (do this after latches have been initialized. OE was held HIGH with pullup during poweron!)
+  digitalWrite(shiftReg_OutputEnable, LOW);
+  pinMode(shiftReg_OutputEnable, OUTPUT);
+  #endif
+ 
+#ifdef USE_HARDWARE_SERIAL  // RS485 via UART Serial, no debug (_debugstream is NULL)
+  Serial.begin(19200, SERIAL_8E1);
+  
+  device = new HBWDevice(HMW_DEVICETYPE, HARDWARE_VERSION, FIRMWARE_VERSION,
+                             &Serial, RS485_TXEN, sizeof(hbwconfig), &hbwconfig,
+                             NUMBER_OF_CHAN, (HBWChannel**)channels,
+                             NULL,
+                             new HBWLinkKey<NUM_LINKS_KEY, LINKADDRESSSTART_KEY>(),
+                             new HBWLinkSwitchSimple<NUM_LINKS_DIM, LINKADDRESSSTART_DIM>()
+                             );
+  
+  device->setConfigPins(BUTTON, LED);  // use analog input for 'BUTTON'
+  
+#else
+  Serial.begin(115200);  // Serial->USB for debug
+  rs485.begin();    // RS485 via SoftwareSerial
+  
+  device = new HBWDevice(HMW_DEVICETYPE, HARDWARE_VERSION, FIRMWARE_VERSION,
+                             &rs485, RS485_TXEN, sizeof(hbwconfig), &hbwconfig,
+                             NUMBER_OF_CHAN, (HBWChannel**)channels,
+                             &Serial,
+                             new HBWLinkKey<NUM_LINKS_KEY, LINKADDRESSSTART_KEY>(),
+                             new HBWLinkSwitchSimple<NUM_LINKS_DIM, LINKADDRESSSTART_DIM>()
+                             );
+  
+  device->setConfigPins(BUTTON, LED);  // 8 (button) and 13 (led) is the default
+
+  hbwdebug(F("B: 2A "));
+  hbwdebug(freeRam());
+  hbwdebug(F(" #num_chan "));
+  //hbwdebug(NUMBER_OF_CHAN);
+  hbwdebug(sizeof(channels)/2);
+
+  // calculate EEPORM start addresses. to put in XML
+  hbwdebug(F(" #eeStartAddr, Dim:0x"));
+  hbwdebughex(0x07);
+  hbwdebug(F(", Key:0x"));
+  hbwdebughex(0x07 + sizeof(hbwconfig.BlDimCfg));
+  hbwdebug(F(", dial:0x"));
+  hbwdebughex(0x07 + sizeof(hbwconfig.BlDimCfg) + sizeof(hbwconfig.DBKeyCfg));
+  hbwdebug(F("\n"));
+#endif
+}
+
+
+void loop()
+{
+  device->loop();
+  POWERSAVE();  // go sleep a bit
+};
---
+++ HBW-Sen-DB-4_dial/HBWPhoneDial.cpp
@@ -0,0 +1,241 @@
+/*
+ * HBWPhoneDial.cpp
+ *
+ * Created (www.loetmeister.de): 25.01.2026
+ */
+
+#include "HBWPhoneDial.h"
+
+// unsigned char PhoneButtons[] = {
+// //byte pos: R1|R2|R3|R4|C4|C3|C2|C1
+//   0b00010010, // 0
+//   0b10000001, // 1
+//   0b10000010, // 2
+//   0b10000100, // 3
+//   0b01000001, // 4
+//   0b01000010, // 5
+//   0b01000100, // 6
+//   0b00100001, // 7
+//   0b00100010, // 8
+//   0b00100100, // 9
+//   0b00010001, // *
+//   0b00010100, // #
+//   0b00011000, // on/off hook
+//   0b01001000, // vol-
+//   0b00101000  // vol+
+// };
+
+// Class HBWPhoneDial
+HBWPhoneDial::HBWPhoneDial(hbw_config_phone_dial* _config, SHIFT_REGISTER_CLASS* _shiftRegister, uint8_t _chan_offset, uint8_t _lineStatePin)
+{
+  config = _config;
+  shiftRegister = _shiftRegister;
+  lineStatePin = _lineStatePin;
+  channelOffset = _chan_offset;
+  // isDialing = false; // TODO change to single viable, dialStatus
+  // callActive = false;
+  lineState = line_status::unknown;
+  phoneState = phone_status::onHook;  // assume idle... lineState will be checked anyway
+  // phonePreviousMillis = 0;
+  // phoneActionDelay = 0;
+
+  pinMode(lineStatePin, INPUT_PULLUP);
+};
+
+void HBWPhoneDial::afterReadConfig()
+{
+  // if (config->suppress_time == 0xF) config->suppress_time = 3;  // 3 seconds
+
+// #ifdef DEBUG_OUTPUT
+  // hbwdebug(F("cfg DBPin:"));
+  // hbwdebug(pin);
+  // hbwdebug(F(" repeat_long_p:"));
+  // hbwdebug(config->repeat_long_press);
+  // hbwdebug(F(" pullup:"));
+  // hbwdebug(config->pullup);
+  // hbwdebug(F("\n"));
+// #endif
+};
+
+
+void HBWPhoneDial::loop(HBWDevice* device, uint8_t channel)
+{
+  // if (config->max_call_time == 0xFF)  return;  // skip locked channels // TODO: enable code
+  
+  // uint32_t now = millis();
+  // if (now == 0) now = 1;  // do not allow time=0 for the below code // AKA  "der Teufel ist ein Eichhoernchen"
+  
+  phoneLoop(); // handle phone actions
+
+  lineState = checkLineState();  // check the status of the phone line
+
+#ifdef DEBUG_OUTPUT_PD
+  static unsigned char lineStateOld = line_status::unknown;
+  if (lineState != lineStateOld) { hbwdebug(F("lineState: ")); hbwdebug(lineState); hbwdebug(F("\n")); lineStateOld = lineState; }
+#endif
+
+};
+
+
+/* initate dialing/calling from other channels of the device */
+bool HBWPhoneDial::DialNumber(uint8_t _index_num)
+{
+  byte chan_calc = _index_num - channelOffset;
+  hbwdebug(F("triggerDial, ch: ")); hbwdebug(chan_calc);
+  if (chan_calc > (uint8_t)sizeof(config->db_mapping)) { hbwdebug(F(" mismatch!\n")); return false; }  // out of range
+  if (config->db_mapping[chan_calc].max_call_duration == 0x1F) { hbwdebug(F(" disabled!\n")); return false; }  // disabled
+
+  if (phoneState != phone_status::onHook || lineState != line_status::idle) {
+    hbwdebug(F(" - busy. DialDelay: ")); hbwdebug(phoneActionDelay);
+    hbwdebug(F(" remaining: ")); hbwdebug(phoneActionDelay - (millis() - phonePreviousMillis));
+    hbwdebug(F("\n"));
+    return false;  // already dialed or active call
+  }
+  phoneState = phone_status::dialing;
+  dbChannel = chan_calc; // store the channel number by which we got triggered
+  setNewPhoneTimer();  // clear timer
+  // lineState = line_status::idle;  // TODO: proper reset?
+  hbwdebug(F(" - ok\n"));
+  return true;
+};
+
+
+void HBWPhoneDial::phoneLoop()
+{
+  if (millis() - phonePreviousMillis >= phoneActionDelay)
+  {
+    setNewPhoneTimer();  // clear timer
+
+    if (phoneState == phone_status::dialing) {
+      // const char num[] = {"123456"};
+      // phoneState = dial(num);
+
+      // TODO: create a way to retreive strings from EEPROM - right now, can only use quick dial
+      char quickDial[] = {"1"};  // create with dummy value... 2 byte string
+      quickDial[0] = (config->db_mapping[dbChannel].db_map +1) + '0';  // lookup quick dial number and convert to string
+      phoneState = dial(quickDial);
+    }
+    else if (phoneState == phone_status::hangupWait || lineState == line_status::terminated) {
+      // max call time reached or call terminated
+      const char num_hook[] = {"7"};  // temp, fixed hook key index number
+      phoneState = dial(num_hook);
+    }
+  }
+};
+
+uint8_t HBWPhoneDial::dial(const char* _numStr)//, bool quickDial)
+{
+  static uint16_t _dialKeyPressAndPauseDelay = 0;
+  static unsigned long _dialPreviousMillis = 0;
+  static bool _buttonPressed = false;
+  static unsigned char _i = 0;
+  static unsigned char _n;
+
+  if (millis() - _dialPreviousMillis >= _dialKeyPressAndPauseDelay) {
+    _dialPreviousMillis = millis();
+
+    // add if (specialButton) ? // like hook or quick dial?
+
+    if (_buttonPressed) {
+      // release button again and wait configured time
+      shiftRegister->setAllLow();
+      _buttonPressed = false;
+      _dialKeyPressAndPauseDelay = DIAL_PRESS_PAUSE_TIME;
+      // if (_numStr[_i] == '<') _dialKeyPressAndPauseDelay = OFF_HOOK_PAUSE_TIME; // fixed pause time... should wait for line signal...?!
+      if (_numStr[_i] == '7')  _dialKeyPressAndPauseDelay = OFF_HOOK_PAUSE_TIME;
+      if (_i <= strlen(_numStr)) _i++;
+      if (_i >= strlen(_numStr)) {  // end of number/dial string
+        _i = 0;
+        _n = 0;
+        _dialKeyPressAndPauseDelay = 0;
+
+        if (phoneState == phone_status::dialing) {
+          // setNewPhoneTimer(HANGUP_AFTER * 1000);  // TODO: config option max call curation
+          setNewPhoneTimer((unsigned long)(config->db_mapping[dbChannel].max_call_duration +1) * 10000);
+          hbwdebug(F("dial done\n"));
+          return phone_status::hangupWait;
+        }
+        else if (phoneState == phone_status::hangupWait || lineState == line_status::terminated) {
+          // lineState = line_status::idle;
+          lineState = line_status::unknown;  // will be checked and set correctly by checkLineState()
+          hbwdebug(F("onHook\n"));
+          return phone_status::onHook;
+        }
+      }
+    }
+    else {
+      // press and hold for configured time
+      _buttonPressed = true;
+      _dialKeyPressAndPauseDelay = DIAL_PRESS_TIME;
+      _n = _numStr[_i] - '0';
+      if (_n < sizeof(PhoneHotkeys)) {
+      // if (_n < sizeof(PhoneButton)) {
+        // shiftRegister->setAll_P(&PhoneButton[n]);
+        shiftRegister->setAll_P(&PhoneHotkeys[_n]);
+        _dialPreviousMillis = millis();  // update timestamp after shift register had been set
+        hbwdebug(F("Dialed: "));//hbwdebug(_numStr[_i]);hbwdebug(F(" arNum: "));hbwdebug(_n);hbwdebug(F("\n"));
+      }
+      else {
+        hbwdebug(F("ERR Dial: "));
+      }  hbwdebug(_numStr[_i]);hbwdebug(F(" arNum: "));hbwdebug(_n);hbwdebug(F("\n"));
+    }
+  }
+  return phoneState;  // no state change
+};
+
+/* check the input pin of the phone line indicator - update 'lineState' based on this indicator and the current call status (phoneState) */
+uint8_t HBWPhoneDial::checkLineState()
+{
+  // pin debounce:
+  static bool line;
+  line = lineIsActive();
+  static bool previousLine = !line; // init with opposite value
+  static unsigned long lineStateCheckMillis = 0;
+
+  if (line == previousLine) {
+    if (!(millis() - lineStateCheckMillis >= LINE_CHANGE_DEBOUNCE))  return lineState;
+  }
+  else {
+    previousLine = line;
+    lineStateCheckMillis = millis();  // restart timer
+    return lineState;
+  }
+
+  // check the status of the phone line
+  switch (lineState)
+  {
+    case line_status::idle: {
+      if ((phoneState == phone_status::dialing || phoneState == phone_status::hangupWait) && lineIsActive())  return line_status::offHookDialing;
+      else if (phoneState == phone_status::onHook && lineIsActive())  return line_status::lineError; // active but we did not even start; TODO set error state and recover (reset phone?)
+      }
+      break;
+    case line_status::offHookDialing: {
+      // wait for lineStatePin to become HIGH (active but reversed poliarity)
+      if (phoneState == phone_status::hangupWait && lineIsReversedPolarity())  return line_status::callEstablished;
+      }
+      break;
+    case line_status::callEstablished: {
+      // wait for lineStatePin to become active again (LOW)
+      if (phoneState == phone_status::hangupWait && lineIsActive()) {
+        // phoneActionDelay = 200;  // need short pause before pressing hook key after termination was detected!
+        // phonePreviousMillis = millis();
+        setNewPhoneTimer(200);  // need short pause before pressing hook key after termination was detected!
+        return line_status::terminated;
+      }
+      if (phoneState == phone_status::onHook && !lineIsActive())  return line_status::idle;  // reset line state when on hook again (max call duration reached)
+      }
+      break;
+    case line_status::terminated: {
+        if (phoneState == phone_status::onHook && !lineIsActive())  return line_status::idle;
+      }
+      break;
+    case line_status::lineError:
+    case line_status::unknown: {
+          if (!lineIsActive()) return line_status::idle;
+      }
+      break;
+    // default:
+    //   return line_status::unknown;
+  }
+  return lineState;  // no change
+};
